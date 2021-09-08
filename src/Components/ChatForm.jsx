f8238e86824a0de1729f17fcc2a87836a7e1b76d
jgrissom <jgrissom91@gmail.com>
2021-09-08 12:17:04 -0500

    [1.07] Controlled form components

--- src/Components/ChatForm.jsx
+++ src/Components/ChatForm.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ChatForm = () => {
+  const [ name, setName ] = useState('');
+  const [ message, setMessage ] = useState('');
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('submit');
   }
-
+  
   return (
     <form onSubmit={handleSubmit} className='Chat-form'>
       <input 
         type="text" 
         id="name" 
+        onChange={ (e) => setName(e.target.value) }
+        name="name"
+        value={name}
         placeholder="Name"
         autoFocus
         autoComplete="off"
@@ -19,6 +25,9 @@ const ChatForm = () => {
       <input
         type="text" 
         id="Message" 
+        onChange={ (e) => setMessage(e.target.value) }
+        name="message"
+        value={message}
         placeholder="Message"
         autoComplete="off"
         autoCapitalize="off">
